a815f77f77bf271638f4a732ab6a879e2346f07c
eliovalenzuela <eliovalenzuela2@gmail.com>
2025-07-22 16:40:09 +0200

    wewdf

--- staubli_tx2_90_description/launch/display.launch.py
+++ staubli_tx2_90_description/launch/display.launch.py
@@ -1,18 +1,21 @@
+# ~/staubli_tx2_90_description/launch/display.launch.py
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import Command, PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     pkg = get_package_share_directory('staubli_tx2_90_description')
     xacro_file = PathJoinSubstitution([pkg, 'urdf', 'staubli_tx2_90_support.urdf.xacro'])
-
+    
     rsp = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
         parameters=[{
-            'robot_description': Command(['xacro ', xacro_file])
+            'robot_description': ParameterValue(
+                Command(['xacro ', xacro_file]), value_type=str)
         }]
     )
     rviz = Node(
@@ -20,5 +23,4 @@ def generate_launch_description():
         executable='rviz2',
         output='screen'
     )
-
     return LaunchDescription([rsp, rviz])
